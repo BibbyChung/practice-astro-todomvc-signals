037f64c52e367dfc9a41030af66f1b8a7255d750
Bibby Chung <bibbynet@gmail.com>
2024-04-14 18:12:29 +0800

    refactor: refactor to trpc service

--- code/astro/src/lib/services/todolist.service.ts
+++ code/astro/src/lib/services/todolist.service.ts
@@ -1,5 +1,6 @@
 import {
   combineLatest,
+  from,
   map,
   of,
   shareReplay,
@@ -7,81 +8,66 @@ import {
   take,
   tap,
 } from "rxjs";
+import type { todoType } from "src/server/todos.router";
+import { trpc } from "../common/trpc";
 import { getBehaviorSubject, getUUID } from "../common/util";
 
 export type todosFilterType = "all" | "active" | "completed";
-
-export type todoType = {
-  id: string;
-  title: string;
-  completed: boolean;
-};
-
-const todos: todoType[] = [
-  {
-    id: "f33f9cd8-4941-4535-bef9-06200b918541",
-    title: "abc",
-    completed: false,
-  },
-];
-
 const todosFilter$ = getBehaviorSubject<todosFilterType>("all");
 export const setTodosFilter = (ff: todosFilterType) => todosFilter$.next(ff);
 export const getTodosFilter = () => todosFilter$.asObservable();
 
-const todos$ = getBehaviorSubject<todoType[]>(todos);
-export const getTodos = () =>
-  getTodosFilter().pipe(
-    switchMap((f) => combineLatest([todos$, of(f)])),
-    map(([todos, f]) => {
-      switch (f) {
-        case "active":
-          return todos.filter((a) => !a.completed);
-        case "completed":
-          return todos.filter((a) => a.completed);
-        default:
-          return todos;
-      }
-    }),
-    shareReplay(1)
-  );
+const refetchTodos = () => setTodosFilter(todosFilter$.value);
+
+const todos$ = getTodosFilter().pipe(
+  switchMap((f) => combineLatest([from(trpc.todos.getAll.query()), of(f)])),
+  map(([todos, f]) => {
+    switch (f) {
+      case "active":
+        return todos.filter((a) => !a.completed);
+      case "completed":
+        return todos.filter((a) => a.completed);
+      default:
+        return todos;
+    }
+  }),
+  shareReplay(1)
+);
+export const getTodos = () => todos$;
 export const addTodo = (title: string) => {
   const todo = {
     title,
     id: getUUID(),
     completed: false,
   };
+  return from(trpc.todos.add.mutate(todo)).pipe(tap(() => refetchTodos()));
+};
+export const delTodo = (id: string) =>
+  from(trpc.todos.del.mutate({ id })).pipe(tap(() => refetchTodos()));
+export const updateTodo = (todo: todoType) =>
+  from(trpc.todos.update.mutate(todo)).pipe(tap(() => refetchTodos()));
 
-  const todos = todos$.value;
-  todos.push(todo);
-  todos$.next(todos);
-};
-export const delTodo = (id: string) => {
-  const todos = todos$.value;
-  const newTodos = todos.filter((a) => a.id !== id);
-  todos$.next(newTodos);
-};
-export const updateTodo = (todo: todoType) => {
-  const todos = todos$.value;
-  const updatedItem = todos.find((a) => a.id === todo.id);
-  if (updatedItem) {
-    Object.assign(updatedItem, todo);
-  }
-  todos$.next(todos);
-};
 export const setAllTodosCompleted = (isCompleted: boolean) =>
   getTodos().pipe(
     take(1),
-    tap((todos) => {
-      todos.forEach((a) => (a.completed = isCompleted));
-      todos$.next(todos);
-    })
+    switchMap((todos) => {
+      const pArr = todos.map((a) => {
+        a.completed = isCompleted;
+        return from(trpc.todos.update.mutate(a));
+      });
+      return combineLatest(pArr);
+    }),
+    tap(() => refetchTodos())
   );
 export const removeAllTodosCompleted = () =>
   getTodos().pipe(
     take(1),
-    tap((todos) => {
-      const newTodos = todos.filter((a) => !a.completed);
-      todos$.next(newTodos);
-    })
+    switchMap((todos) => {
+      const newTodos = todos.filter((a) => a.completed);
+      const pArr = newTodos.map((todo) =>
+        from(trpc.todos.del.mutate({ id: todo.id }))
+      );
+      return combineLatest(pArr);
+    }),
+    tap(() => refetchTodos())
   );
