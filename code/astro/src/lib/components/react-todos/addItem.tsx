49c21fb7ef04f29212b242564b7e0615f8e8fb85
Bibby Chung <bibbynet@gmail.com>
2024-11-20 21:32:11 +0800

    refactor

--- code/astro/src/lib/components/react-todos/addItem.tsx
+++ code/astro/src/lib/components/react-todos/addItem.tsx
@@ -1,44 +1,46 @@
-import { useEffect, useRef } from "react";
-import { tap } from "rxjs";
-import { useSubject } from "~/lib/common/rxjs-interop-react";
-import { addTodo } from "~/lib/services/todolist.service";
+import { useEffect, useRef } from 'react'
+import { tap } from 'rxjs'
+import { useSubject } from '~/lib/common/rxjs-interop-react'
+import { addTodo } from '~/lib/services/todolist.service'
 
 export default function AddItem() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const formSubmitForm$ = useSubject<boolean>();
+  const inputRef = useRef<HTMLInputElement>(null)
+  const formSubmitForm$ = useSubject<boolean>()
 
   useEffect(() => {
-    const formSubmitSub = formSubmitForm$.pipe(
-      tap(() => {
-        const v = inputRef.current?.value ?? '';
-        if (v !== '' && inputRef.current) {
-          addTodo(v ?? '');
-          inputRef.current.value = "";
-        }
-      })
-    ).subscribe();
+    const formSubmitSub = formSubmitForm$
+      .pipe(
+        tap(() => {
+          const v = inputRef.current?.value ?? ''
+          if (v !== '' && inputRef.current) {
+            addTodo(v ?? '')
+            inputRef.current.value = ''
+          }
+        })
+      )
+      .subscribe()
 
     return () => {
-      formSubmitSub.unsubscribe();
-    };
-  }, []);
+      formSubmitSub.unsubscribe()
+    }
+  }, [])
 
   return (
-    <header className="header">
+    <header className='header'>
       <h1>todos</h1>
       <form
         onSubmit={(e) => {
-          formSubmitForm$.next(true);
-          e.preventDefault();
+          formSubmitForm$.next(true)
+          e.preventDefault()
         }}
       >
         <input
-          className="new-todo"
-          placeholder="What needs to be done?"
+          className='new-todo'
+          placeholder='What needs to be done?'
           autoFocus={true}
           ref={inputRef}
         />
       </form>
     </header>
-  );
+  )
 }
--- code/astro/src/lib/services/todolist.service.ts
+++ code/astro/src/lib/services/todolist.service.ts
@@ -1,87 +1,79 @@
-import {
-  combineLatest,
-  map,
-  of,
-  shareReplay,
-  switchMap,
-  take,
-  tap,
-} from "rxjs";
-import { getBehaviorSubject, getUUID } from "../common/util";
+import { combineLatest, map, of, shareReplay, switchMap, take, tap } from 'rxjs'
+import { getBehaviorSubject, getUUID } from '../common/util'
 
-export type todosFilterType = "all" | "active" | "completed";
+export type todosFilterType = 'all' | 'active' | 'completed'
 
 export type todoType = {
-  id: string;
-  title: string;
-  completed: boolean;
-};
+  id: string
+  title: string
+  completed: boolean
+}
 
 const todos: todoType[] = [
   {
-    id: "f33f9cd8-4941-4535-bef9-06200b918541",
-    title: "abc",
+    id: 'f33f9cd8-4941-4535-bef9-06200b918541',
+    title: 'abc',
     completed: false,
   },
-];
+]
 
-const todosFilter$ = getBehaviorSubject<todosFilterType>("all");
-export const setTodosFilter = (ff: todosFilterType) => todosFilter$.next(ff);
-export const getTodosFilter = () => todosFilter$.asObservable();
+const todosFilter$ = getBehaviorSubject<todosFilterType>('all')
+export const setTodosFilter = (ff: todosFilterType) => todosFilter$.next(ff)
+export const getTodosFilter = () => todosFilter$.asObservable()
 
-const todos$ = getBehaviorSubject<todoType[]>(todos);
+const todos$ = getBehaviorSubject<todoType[]>(todos)
 export const getTodos = () =>
   getTodosFilter().pipe(
     switchMap((f) => combineLatest([todos$, of(f)])),
     map(([todos, f]) => {
       switch (f) {
-        case "active":
-          return todos.filter((a) => !a.completed);
-        case "completed":
-          return todos.filter((a) => a.completed);
+        case 'active':
+          return todos.filter((a) => !a.completed)
+        case 'completed':
+          return todos.filter((a) => a.completed)
         default:
-          return todos;
+          return todos
       }
     }),
     shareReplay(1)
-  );
+  )
 export const addTodo = (title: string) => {
   const todo = {
     title,
     id: getUUID(),
     completed: false,
-  };
-
-  const todos = todos$.value;
-  todos.push(todo);
-  todos$.next(todos);
-};
-export const delTodo = (id: string) => {
-  const todos = todos$.value;
-  const newTodos = todos.filter((a) => a.id !== id);
-  todos$.next(newTodos);
-};
-export const updateTodo = (todo: todoType) => {
-  const todos = todos$.value;
-  const updatedItem = todos.find((a) => a.id === todo.id);
-  if (updatedItem) {
-    Object.assign(updatedItem, todo);
   }
-  todos$.next(todos);
-};
+
+  const todos = todos$.value
+  todos.push(todo)
+  todos$.next(todos)
+}
+export const delTodo = (id: string) => {
+  const todos = todos$.value
+  const newTodos = todos.filter((a) => a.id !== id)
+  todos$.next(newTodos)
+}
+export const updateTodo = (todo: todoType) => {
+  const todos = todos$.value
+  const updatedItem = todos.find((a) => a.id === todo.id)
+  if (updatedItem) {
+    Object.assign(updatedItem, todo)
+  }
+  todos$.next(todos)
+}
 export const setAllTodosCompleted = (isCompleted: boolean) =>
   getTodos().pipe(
     take(1),
     tap((todos) => {
-      todos.forEach((a) => (a.completed = isCompleted));
-      todos$.next(todos);
+      todos.forEach((a) => (a.completed = isCompleted))
+      todos$.next(todos)
     })
-  );
+  )
 export const removeAllTodosCompleted = () =>
   getTodos().pipe(
     take(1),
     tap((todos) => {
-      const newTodos = todos.filter((a) => !a.completed);
-      todos$.next(newTodos);
+      const newTodos = todos.filter((a) => !a.completed)
+      todos$.next(newTodos)
     })
-  );
+  )
