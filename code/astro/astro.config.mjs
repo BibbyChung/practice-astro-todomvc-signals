c2a42fb716465dbb1d75ad00c90a01277083fdfa
Bibby Chung <bibbynet@gmail.com>
2024-11-21 03:30:19 +0800

    feat: add vercel setting

--- code/astro/astro.config.mjs
+++ code/astro/astro.config.mjs
@@ -1,29 +1,25 @@
-import { defineConfig } from 'astro/config'
-import svelte from '@astrojs/svelte'
-import react from '@astrojs/react'
-import UnoCSS from 'unocss/astro'
-import node from '@astrojs/node'
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
+import UnoCSS from 'unocss/astro';
+import node from '@astrojs/node';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
-  integrations: [
-    svelte(),
-    react(),
-    UnoCSS({
-      injectReset: true,
-      configFile: './uno.config.ts',
-    }),
-  ],
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  integrations: [svelte(), react(), UnoCSS({
+    injectReset: true,
+    configFile: './uno.config.ts'
+  })],
+  output: 'server',
+  adapter: vercel(),
   // github pages
   // https://www.cmsoftdev.com/01-github-deploy/
   build: {
-    assets: 'astro',
-  },
-})
+    assets: 'assets'
+  }
+});
