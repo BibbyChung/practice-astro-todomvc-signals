ae64b3872d91c8390a93e037457936d8747b6fb4
Bibby Chung <bibbynet@gmail.com>
2024-11-20 18:23:10 +0800

    feat: add environment and gh-pages features
    # Conflicts:
    #	code/astro/astro.config.mjs
    #	code/astro/pnpm-lock.yaml
    #	code/astro/src/lib/components/menu.svelte

--- code/astro/astro.config.mjs
+++ code/astro/astro.config.mjs
@@ -1,18 +1,26 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import react from "@astrojs/react";
-import analogjsangular from "@analogjs/astro-angular";
-import UnoCSS from "unocss/astro";
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import react from '@astrojs/react'
+import analogjsangular from '@analogjs/astro-angular'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   integrations: [
     svelte(),
     react(),
     analogjsangular(),
     UnoCSS({
-      configFile: "./uno.config.ts",
+      configFile: './uno.config.ts',
       injectReset: true,
     }),
   ],
-});
+  // github pages
+  // https://www.cmsoftdev.com/01-github-deploy/
+  build: {
+    assets: 'astro',
+  },
+})
